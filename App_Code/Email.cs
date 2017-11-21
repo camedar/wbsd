f2b37303afc7096ba2ff8c3c66a8b19a780e0454
Camilo Medina <m.camilo@gmail.com>
2017-11-21 22:08:22 +0000

    remove email credentials from source code

---
+++ App_Code/Email.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+/// <summary>
+/// Summary description for email
+/// </summary>
+public class Email
+{
+    private string from = "email";
+    private string password = "password";
+    private string to = null;
+    private string smtp = "smtp.gmail.com";
+    private int port = 587;
+
+    public Email()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public bool send(string recipientEmail,string subject,string textMessage)
+    {
+        try
+        {
+            MailMessage message = new MailMessage(from, recipientEmail, subject, textMessage);
+            message.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient(smtp, 587);
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential(from,password);
+            client.Send(message);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.Message.ToString());
+            return false;
+        }
+    }
+}
